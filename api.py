9a0e4c1fe3d61e5694fb36866b65efc172328694
A.M. Ali <a.m.ali@Mac.lan>
2026-04-12 07:59:18 -0400

    set allow_credentials false for cors

--- api.py
+++ api.py
@@ -16,7 +16,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # allow all (we tighten later)
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
