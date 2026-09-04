7a47a23d0be6bd9fdbacae26051d4c5c20c48008
Sovereign Intelligence <alimmohammad80@gmail.com>
2026-09-04 17:56:34 -0400

    Export CERT and MISP collectors

--- app/cyber_information/collectors/__init__.py
+++ app/cyber_information/collectors/__init__.py
@@ -1,9 +1,11 @@
 """Authoritative live-source collectors for Cyber & Information Operations."""
 
 from .abuseipdb import AbuseIpDbCollector
+from .cert_feeds import CertFeedCollector, cert_feed_registry
 from .cisa_advisories import CisaAdvisoryCollector
 from .cisa_kev import CisaKevCollector
 from .gdelt import GdeltCollector
+from .misp import MispCollector
 from .mitre_attack import MitreAttackCollector
 from .nvd import NvdCollector
 from .stix_taxii import StixBundleAdapter, TaxiiCollectionCollector
@@ -11,9 +13,12 @@ from .urlhaus import UrlhausCollector
 
 __all__ = [
     "AbuseIpDbCollector",
+    "CertFeedCollector",
+    "cert_feed_registry",
     "CisaAdvisoryCollector",
     "CisaKevCollector",
     "GdeltCollector",
+    "MispCollector",
     "MitreAttackCollector",
     "NvdCollector",
     "StixBundleAdapter",
