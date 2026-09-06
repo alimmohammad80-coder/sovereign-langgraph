d2172c8916cd1e697a1d437e917ee98a54dd6233
Sovereign Intelligence <alimmohammad80@gmail.com>
2026-09-05 20:57:44 -0400

    Add financial corporate service package

---
+++ services/financial_corporate/__init__.py
@@ -0,0 +1,6 @@
+"""Core services for Financial & Corporate Risk Intelligence."""
+
+from .entity_master import CorporateEntityMaster
+from .risk_engine import CorporateRiskEngine
+
+__all__ = ["CorporateEntityMaster", "CorporateRiskEngine"]
