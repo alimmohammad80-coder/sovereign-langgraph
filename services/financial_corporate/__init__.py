554ff119e5b13940ca70187bc48dc0edd5287271
Sovereign Intelligence <alimmohammad80@gmail.com>
2026-09-05 21:07:37 -0400

    Export corporate universe service

--- services/financial_corporate/__init__.py
+++ services/financial_corporate/__init__.py
@@ -5,11 +5,13 @@ from .fundamentals import CorporateFundamentalsAnalyzer
 from .gleif import GLEIFCollector
 from .risk_engine import CorporateRiskEngine
 from .sec_edgar import SECEdgarCollector, SECConfigurationError
+from .universe import CorporateUniverseService
 
 __all__ = [
     "CorporateEntityMaster",
     "CorporateFundamentalsAnalyzer",
     "CorporateRiskEngine",
+    "CorporateUniverseService",
     "GLEIFCollector",
     "SECEdgarCollector",
     "SECConfigurationError",
